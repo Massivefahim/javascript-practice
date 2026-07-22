e7788f77918a00b90d95510d21691ed02fc332bb
Mobashshar Fahim <mobasharfahim@gmail.com>
2026-07-22 18:03:21 +0530

    Add JavaScript data types example

---
+++ 01-Basics/datatypes.js
@@ -0,0 +1,13 @@
+// JavaScript Data Types
+
+let name = "Mobashshar";
+let age = 23;
+let isStudent = true;
+let salary = null;
+let city;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof salary);
+console.log(typeof city);
