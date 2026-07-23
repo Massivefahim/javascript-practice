f57caf256fc7aa5d2ffb1f4ead644bc55b3100a5
Mobashshar Fahim <mobasharfahim@gmail.com>
2026-07-23 17:59:51 +0530

    Add JavaScript operators example

---
+++ 01-Basics/operators.js
@@ -0,0 +1,10 @@
+// JavaScript Operators
+
+let a = 20;
+let b = 10;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
