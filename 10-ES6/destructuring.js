36d3043c428d516a2821381e022af4422f2b1004
Mobashshar Fahim <mobasharfahim@gmail.com>
2026-08-06 23:58:31 +0530

    Add object destructuring example

---
+++ 10-ES6/destructuring.js
@@ -0,0 +1,13 @@
+// Object Destructuring
+
+const student = {
+    name: "Mobashshar",
+    age: 23,
+    course: "B.Tech CSE"
+};
+
+const { name, age, course } = student;
+
+console.log(name);
+console.log(age);
+console.log(course);
