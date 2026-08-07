fd489d2e102ca5c4644aa5ac176349a801159c66
Mobashshar Fahim <mobasharfahim@gmail.com>
2026-08-08 00:01:56 +0530

    Add enhanced object literals example

---
+++ 10-ES6/enhanced-object.js
@@ -0,0 +1,16 @@
+// Enhanced Object Literals
+
+const name = "Mobashshar";
+const age = 23;
+
+const student = {
+    name,
+    age,
+
+    greet() {
+        console.log("Welcome!");
+    }
+};
+
+console.log(student);
+student.greet();
